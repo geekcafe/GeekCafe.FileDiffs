ff82d7ac2dbb9d1d5f10c2db4027b666e80ad565
Eric Wilson <eric.wilson@geekcafe.com>
2020-03-28 15:26:49 -0400

    ✨ feature(FileDiffSercice)

---
+++ src/GeekCafe.FileDiffs.Service/DirectoryCompareService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace GeekCafe.FileDiffs.Service
+{
+    public class DirectoryCompareService
+    {
+        public DirectoryCompareService()
+        {
+        }
+
+        public static bool IsEqual(string pathLeft, string pathRight)
+        {
+            var dirsLeft = System.IO.Directory.GetDirectories(pathLeft);
+            var dirsRight = System.IO.Directory.GetDirectories(pathRight);
+
+            if (dirsLeft.Length != dirsRight.Length)
+            {
+                return false;
+            }
+
+            return false;
+        }
+}
---
+++ src/GeekCafe.FileDiffs.Service/FileCompareService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace GeekCafe.FileDiffs.Service
+{
+    public class FileCompareService
+    {
+        
+        public static bool IsEqual(string pathLeft, string pathRight)
+        {
+            byte[] fileLeft = File.ReadAllBytes(pathLeft);
+            byte[] fileRight = File.ReadAllBytes(pathRight);
+            if (fileLeft.Length == fileRight.Length)
+            {
+                for (int i = 0; i < fileLeft.Length; i++)
+                {
+                    if (fileLeft[i] != fileRight[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
